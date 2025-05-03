677f353db618801617e421248330292a68388bb4
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-03 10:24:42 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+import org.lesson.java.shop.Product;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product tv= new Product("WOo che tv", "LG", new BigDecimal(120.20), 0);
+        System.out.println(tv.getIva());
+
+
+
+    }
+}
---
+++ src/org/lesson/java/shop/Product.java
@@ -0,0 +1,68 @@
+package org.lesson.java.shop;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Product {
+
+    private String name;
+    private int code;
+    private String brand;
+    private BigDecimal price;
+    private float iva;
+
+    public Product(String name, String brand, BigDecimal price, float iva) {
+        Random rand = new Random();
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        if (this.iva == 0) {
+            this.iva = 0.22f;
+        } else {
+            this.iva = iva;
+        }
+
+        this.code=rand.nextInt(9999);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String updatedName) {
+        this.name = updatedName;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    private void setCode(int updatedCode) {
+        this.code = updatedCode;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String updatedBrand) {
+        this.brand = updatedBrand;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal updatedPrice) {
+        this.price = updatedPrice;
+    }
+
+    public float getIva() {
+        return this.iva;
+    }
+
+    public void setIva(float updatedIva) {
+        this.iva = updatedIva;
+    }
+
+}
